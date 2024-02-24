c88b4c00172dc6d32e1e7b8f5fe045b431de08cd
Sanket <snktshegokar876@gmail.com>
2024-02-25 04:30:49 +0530

    added User Profile feed API

--- profile_api/urls.py
+++ profile_api/urls.py
@@ -10,6 +10,7 @@ from .views import fetch_tweets
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 router.register('profile', views.UserProfileViewSet )
+router.register('feed', views.UserProfileFeedViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
--- profile_api/views.py
+++ profile_api/views.py
@@ -6,15 +6,17 @@ from rest_framework import status
 
 from rest_framework import viewsets
 
-from profile_api import models
-
 from rest_framework.authentication import TokenAuthentication
 
 from rest_framework.authtoken.views import ObtainAuthToken
 
 from rest_framework.settings import api_settings
 
+from rest_framework.permissions import IsAuthenticated
+
 from profile_api import permissions
+
+from profile_api import models
 
 from rest_framework import filters
 
@@ -130,6 +132,17 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 class UserLoginApiView(ObtainAuthToken):
    """Handle creating user authentication tokens"""
    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES    
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.UpdateOwnStatus,IsAuthenticated)
+    
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
 
 def fetch_tweets(request):
     # Twitter API credentials
