c193e4b1ef892906d253a8f2257829660dd443b1
Sanket <snktshegokar876@gmail.com>
2024-02-24 22:54:57 +0530

    added Login API with token based authentication

--- profile_api/views.py
+++ profile_api/views.py
@@ -10,6 +10,10 @@ from profile_api import models
 
 from rest_framework.authentication import TokenAuthentication
 
+from rest_framework.authtoken.views import ObtainAuthToken
+
+from rest_framework.settings import api_settings
+
 from profile_api import permissions
 
 from rest_framework import filters
@@ -122,6 +126,10 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+class UserLoginApiView(ObtainAuthToken):
+   """Handle creating user authentication tokens"""
+   renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES    
 
 def fetch_tweets(request):
     # Twitter API credentials
